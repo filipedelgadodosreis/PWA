1c67ef0cb6bcb9281bcdc1fd7ef2e9a3759bd033
filipe <filipe.reis@verity.com.br>
2019-10-05 20:38:21 -0300

    Inserção de mensagem quando sistema está offline.

--- BlogPWA/wwwroot/js/blogService.js
+++ BlogPWA/wwwroot/js/blogService.js
@@ -4,18 +4,40 @@
         var oldestBlogPostId = 0;
         var blogPostUrl = '/Home/Post/?link=';
         var blogLatestPostsUrl = '/Home/LatestBlogPosts/';
-        var blogMorePostsUrl = '/Home/MoreBlogPosts/?oldestBlogPostId=';        function loadData(url) {
-            fetch(url)
-                .then(function (response) {
-                    return response.json();
-                }).then(function (data) {
-                    template.appendBlogList(data);
-                    setOldestBlogPostId(data);
+        var blogMorePostsUrl = '/Home/MoreBlogPosts/?oldestBlogPostId=';
+
+
+        function fetchPromise(url) {
+            return new Promise(function (resolve, reject) {
+                fetch(url)
+                    .then(function (response) {
+                        return response.json();
+                    }).then(function (data) {
+                        template.appendBlogList(data);
+                        setOldestBlogPostId(data);
+                        resolve('The connection is OK, showing latest results');
+                    }).catch(function (e) {
+                        resolve('No connection, showing offline results');
+                    });
+                setTimeout(function () {
+                    resolve('The connection is hanging, showing offline results');
+                }, 5000);
+            });
+        }
+
+        function loadData(url) {
+            fetchPromise(url)
+                .then(function (status) {
+                    $('#connection-status').html(status);
                 });
-        }        function setOldestBlogPostId(data) {
+        }
+
+
+        function setOldestBlogPostId(data) {
             var ids = data.result.data.map(item => item.postId);
             oldestBlogPostId = Math.min(...ids);
-        }
+        }
+
         function loadBlogPost(link) {
             fetch(blogPostUrl + link)
                 .then(function (response) {
@@ -31,13 +53,15 @@
 
         function loadLatestBlogPosts() {
             loadData(blogLatestPostsUrl);
-        }
+        }
+
         function loadMoreBlogPosts() {
             loadData(blogMorePostsUrl + oldestBlogPostId);
-        }
+        }
+
         return {
             loadLatestBlogPosts: loadLatestBlogPosts,
             loadBlogPost: loadBlogPost,
             loadMoreBlogPosts: loadMoreBlogPosts
         }
-    });
+    });
